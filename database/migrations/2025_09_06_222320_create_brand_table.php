e92989fedd0c9aeb2ef9e8e55f8026c6968f99bd
Wizzar <juniorminano080@gmail.com>
2025-09-06 17:27:41 -0500

    Se agrego la tabla brand con sus datos

--- database/migrations/2025_09_06_222320_create_brand_table.php
+++ database/migrations/2025_09_06_222320_create_brand_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('category', function (Blueprint $table) {
+        Schema::create('brand', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description'); //Mas de 255 caraceteres
+            $table->text('description');
             $table->string('img');
-            $table->string('slug')->unique();
-            $table->boolean('status')->default(true); //Activo por defecto
+            $table->string('slug');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('category');
+        Schema::dropIfExists('brand');
     }
 };
