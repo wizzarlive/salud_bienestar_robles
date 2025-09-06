749b8f5d498e177511fb315c1eeaea3b5c6cf8e9
Wizzar <juniorminano080@gmail.com>
2025-09-06 17:37:14 -0500

    Se agrego la tabla products con sus datos

---
+++ database/migrations/2025_09_06_223051_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img');
+            $table->decimal('price',8,2);
+            $table->string('sku')->unique();
+            $table->integer('stock');
+            $table->text('description');
+            $table->decimal('discount_price',8,2)->nullable();
+            $table->boolean('status')->default(true);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
