c702ecd6dcff11c40e082a5c80c913e0cde06f98
Wizzar <juniorminano080@gmail.com>
2025-09-10 10:44:13 -0500

    Se guardo todo

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
+use Laravel\Socialite\Facades\Socialite;
 
-// Páginas estáticas
 Route::view('contacto', 'contacto');
 Route::view('header', 'header');
 Route::view('footer', 'footer');
 Route::view('nosotros', 'nosotros');
 
-// CRUD de usuarios
+
 Route::resource('users', UserController::class);
 
-// Dashboard protegido
 Route::middleware(['auth'])->get('/dashboard', function () {
     return view('dashboard');
 });
 
-// Autenticación (Laravel UI)
 Auth::routes();
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// Login con Google (Socialite)
-Route::get('login/google', [LoginController::class, 'redirectToGoogle']);
-Route::get('login/google/callback', [LoginController::class, 'handleGoogleCallback']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle']);
+Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
+
