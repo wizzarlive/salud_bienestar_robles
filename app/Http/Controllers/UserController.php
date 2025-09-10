01986132d0bb8cb2ee275578f078cfc221613186
Wizzar <juniorminano080@gmail.com>
2025-09-10 10:04:18 -0500

    Loign google

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,8 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all(); // Trae todos los usuarios
-        return response()->json($users); // Devuelve como JSON
+        //$users = User::all(); // Trae todos los usuarios
+        //return response()->json($users); // Devuelve como JSON
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('contacto', function () {
     return view('contacto');
@@ -21,4 +22,21 @@ Route::get('nosotros', function () {
 
 Route::resource('users', UserController::class);
 
+Route::middleware(['auth'])->get('/dashboard', function () {
+    return view('dashboard');
+});
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle']);
+Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
