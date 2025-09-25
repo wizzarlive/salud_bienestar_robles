396006143014593cfc206242228f2cebd7a4a42a
Wizzar <juniorminano080@gmail.com>
2025-09-24 19:34:41 -0500

    Se agrego el auth.php en lan es

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas se utilizan durante la autenticación
+    | para varios mensajes que necesitamos mostrar al usuario.
+    |
+    */
+
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente de nuevo en :seconds segundos.',
+
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las que se devuelven por el broker
+    | de contraseñas al intentar actualizar la contraseña de un usuario.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo electrónico el enlace de restablecimiento de contraseña!',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+
+];
