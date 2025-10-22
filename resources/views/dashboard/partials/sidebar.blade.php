e54cb93c93fac79a3d5601a70a0f981e1e92ed77
Wizzar <juniorminano080@gmail.com>
2025-10-22 16:51:39 -0500

    Se termino el diseño de sidbar

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,39 +1,85 @@
-<div>
-    <div>
-        <img src="" alt="">
-        <img src="" alt="">
+<div class="barra-sidebar">
+
+    <div class="barra">
+        <div class="block-1">
+            <img src="{{ Vite::asset('resources/img/sidebar/logo.webp') }}" alt="logo">
+            <img src="{{ Vite::asset('resources/img/sidebar/icon.webp') }}" alt="icon">
+        </div>
+
+        <div class="block-2">
+
+            <a href="">
+                <div class="menu-list">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/sidebar/profile.webp') }}" alt="profile">
+
+                    </div>
+                    <div>
+                        <p>Perfil</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="menu-list">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/sidebar/product.webp') }}" alt="product">
+
+                    </div>
+                    <div>
+                        <p>Productos</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="menu-list">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/sidebar/boletas.webp') }}" alt="boletas">
+
+                    </div>
+                    <div>
+                        <p>Boletas</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="menu-list">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/sidebar/mensaje.webp') }}" alt="mensaje">
+
+                    </div>
+                    <div>
+                        <p>Mensaje</p>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="menu-list">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/sidebar/usuario.webp') }}" alt="usuario">
+
+                    </div>
+                    <div>
+                        <p>Usuarios</p>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 
-    <div>
-        <div>
-            <img src="" alt="">
-            <p>Perfil</p>
-        </div>
 
-        <div>
-            <img src="" alt="">
-            <p>Perfil</p>
-        </div>
-
-        <div>
-            <img src="" alt="">
-            <p>Perfil</p>
-        </div>
-
-        <div>
-            <img src="" alt="">
-            <p>Perfil</p>
-        </div>
-
-        <div>
-            <img src="" alt="">
-            <p>Perfil</p>
-        </div>
-    </div>
-
-    <div>
-        <a href="">
-            Cerrar Sesión
+    <div class="logout">
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="logout-link">
+            <img src="{{ Vite::asset('resources/img/sidebar/leave.webp') }}" alt="leave">
+            <span>Cerrar sesión</span>
         </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
+            @csrf
+        </form>
     </div>
 </div>
